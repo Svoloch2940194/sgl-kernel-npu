320900b13aa83a40c15df5a49a00fac9f7a0ba6e
Seraphim Volochaev <116020688+Svoloch2940194@users.noreply.github.com>
2026-09-10 01:55:26 -0700

    Add LoRA rank = 8

--- tests/python/sgl_kernel_npu/test_lora_kernels.py
+++ tests/python/sgl_kernel_npu/test_lora_kernels.py
@@ -12,12 +12,12 @@ torch.set_printoptions(threshold=float("inf"))
 class TestLoraKernels(unittest.TestCase):
     def test_sgemmv_shrink(self):
         batch_size = 2
-        input_dim = 1024
+        input_dim = 2*1024
         num_loras = 3
         dtype = torch.float16
         device_dtype = torch.float16
 
-        possible_lora_ranks = [16, 32, 64]
+        possible_lora_ranks = [8, 16, 32, 64]
         lora_ranks = random.sample(
             possible_lora_ranks,
             counts=[num_loras] * len(possible_lora_ranks),
@@ -84,12 +84,12 @@ class TestLoraKernels(unittest.TestCase):
 
     def test_sgemmv_expand(self):
         batch_size = 4
-        output_dim = 1024
+        output_dim = 2*1024
         num_loras = 8
         dtype = torch.float16
         device_dtype = torch.float16
 
-        possible_lora_ranks = [16, 32, 64]
+        possible_lora_ranks = [8, 16, 32, 64]
         lora_ranks = random.sample(
             possible_lora_ranks,
             counts=[num_loras] * len(possible_lora_ranks),
